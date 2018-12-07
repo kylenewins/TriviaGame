711d8f62d001c56e30fedab26e70215483ef35b7
Toad Sage <kylenewins@gmail.com>
2018-12-06 21:17:52 -0500

    almost done, still needs style and content adjustments

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,23 +2,42 @@ $(document).ready(function(){
 //global variables
 var correct = 0;
 var incorrect = 0;
-var number = 10;
+var unanswered = 0;
+var number = 21;
 var timerInt;
 
-var labels = ["first", "second", "third", "forth", "fifth"];
-
-//gamestart function
+//gamestart function resets to the default state, is run once at the beginning and then on reset
 var gameStart = function(){
+    //correct and incorrect values/timer reset
     correct=0;
     incorrect=0;
-    number = 10;
+    unanswered=0;
+    number = 21;
 
+    //clears the timer interval and empties the html div
     clearInterval(timerInt);
     $("#timer").empty();
 
+    //sets which div by default shows up first
     $("#start-box").show();
     $("#quiz-box").hide();
     $("#end-box").hide();
+
+    //unchecks all of the radio inputs
+    $("#Ques1True").prop('checked', false);
+    $("#Ques1False").prop('checked', false);
+
+    $("#Ques2True").prop('checked', false);
+    $("#Ques2False").prop('checked', false);
+
+    $("#Ques3True").prop('checked', false);
+    $("#Ques3False").prop('checked', false);
+
+    $("#Ques4True").prop('checked', false);
+    $("#Ques4False").prop('checked', false);
+
+    $("#Ques5True").prop('checked', false);
+    $("#Ques5False").prop('checked', false);
 }
 
 //show the quiz page and starts the timer on start button click
@@ -28,9 +47,11 @@ $("#start-button").on("click", function(){
     run();
 })
 
-//function to display the questions
+//display the questions
+$("#timerText").text("Time Remaining: ");//rewrites the number in the #quiz-box element.
 
-//interval timer at the top of quiz page
+
+//display interval timer at the top of quiz page
 function run() {
     clearInterval(timerInt);
     timerInt = setInterval(decrease, 1000);
@@ -40,7 +61,7 @@ function run() {
     
     number--; //  Decreases number by one.
     
-    $("#timer").html("<h2>" + "Time Remaining: " + number + "</h2>");//rewrites the number in the #quiz-box element.
+    $("#timerText").text("Time Remaining: " + number);//rewrites the number in the #quiz-box element.
 
     if (number === 0) { //  Once number hits zero...
 
@@ -54,16 +75,51 @@ function run() {
 $("#sub-button").on("click", function(){
     $("#quiz-box").hide();
     $("#end-box").show();
+
+    //Button Checks
+    //Question 1
+    if($('#Ques1True').is(':checked')) { correct++}
+    else if ($('#Ques1False').is(':checked')) { incorrect++}
+    else if(($("#Ques1True").prop('checked', false)) && ($("#Ques1False").prop('checked', false))) {
+        unanswered++
+    };
+    //Question 2
+    if($('#Ques2True').is(':checked')) { incorrect++}
+    else if($('#Ques2False').is(':checked')) { correct++}
+    else if(($("#Ques2True").prop('checked', false)) && ($("#Ques2False").prop('checked', false))) {
+        unanswered++
+    };
+    //Question 3
+    if($('#Ques3True').is(':checked')) { correct++ }
+    else if($('#Ques3False').is(':checked')) { incorrect++ }
+    else if(($("#Ques3True").prop('checked', false)) && ($("#Ques3False").prop('checked', false))) {
+        unanswered++
+    };
+    //Question 4
+    if($('#Ques4True').is(':checked')) { incorrect++ }
+    else if($('#Ques4False').is(':checked')) { correct++ }
+    else if(($("#Ques4True").prop('checked', false)) && ($("#Ques4False").prop('checked', false))) {
+        unanswered++
+    };
+    //Question 5
+    if($('#Ques5True').is(':checked')) { correct++ }
+    else if($('#Ques5False').is(':checked')) { incorrect++ }
+    else if(($("#Ques5True").prop('checked', false)) && ($("#Ques5False").prop('checked', false))) {
+        unanswered++
+    };
+
+    $("#totalCorrect").text("Correct Answers: " + correct);
+    $("#totalIncorrect").text("Incorrect Answers: " + incorrect);
+    $("#unanswered").text("Unanswered: " + unanswered);
 })
+
 
 //gameEnd page
 $("#reset-button").on("click", function(){
     gameStart();
 })
 
-
-
-
+console.log($("#Ques1True").checked)
 gameStart();
     
 });//end of document ready
